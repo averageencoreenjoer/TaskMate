3f60035a78c082b0323d81b783675908ad2e6c10
polputiotsuda <qudomibusiness@gmail.com>
2024-12-08 05:52:51 +0300

    Добавлены файлы проекта

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Task
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{'id': task.id, 'title': task.title, 'done': task.done} for task in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'id': new_task.id, 'title': new_task.title, 'done': new_task.done}), 201
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    return jsonify({'id': task.id, 'title': task.title, 'done': task.done})
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    data = request.get_json()
+    task.title = data.get('title', task.title)
+    task.done = data.get('done', task.done)
+    db.session.commit()
+    return jsonify({'id': task.id, 'title': task.title, 'done': task.done})
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
